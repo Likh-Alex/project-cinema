7cd58279baafb7f7d674ae5c6524409994034cac
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-30 15:01:55 +0300

    Moved the movieService from the main method

--- src/main/java/ua/mate/cinema/Main.java
+++ src/main/java/ua/mate/cinema/Main.java
@@ -6,15 +6,13 @@ import ua.mate.cinema.service.MovieService;
 
 public class Main {
     private static Injector injector = Injector.getInstance("ua.mate.cinema");
+    private static MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
 
     public static void main(String[] args) {
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.getAll().forEach(System.out::println);
-
         Movie movie = new Movie();
         movie.setTitle("Tor 7");
         movieService.add(movie);
-
         movieService.getAll().forEach(System.out::println);
     }
 }
