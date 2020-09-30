aab41ad13522cd6006b2b0e0d6b199284b0a7941
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-30 16:34:03 +0300

    Added annotation in Movie class

--- src/main/java/ua/mate/cinema/model/Movie.java
+++ src/main/java/ua/mate/cinema/model/Movie.java
@@ -6,11 +6,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "movie")
+@Table(name = "movies")
 public class Movie {
     @Id
     @Column(name = "movie_id")
